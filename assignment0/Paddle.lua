90da10434b5634a75082efd046f69e5bf9c79fd8
alice <bow1226@gmail.com>
2020-01-21 14:24:47 +0800

    docs: Add TODO task

--- assignment0/Paddle.lua
+++ assignment0/Paddle.lua
@@ -60,3 +60,13 @@ end
 function Paddle:render()
     love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
 end
+
+--[[
+    TODO Implement an AI-controlled paddle (either the left or the right will do) 
+        such that it will try to deflect the ball at all times. 
+        Since the paddle can move on only one axis (the Y axis), 
+        you will need to determine how to keep the paddle moving in relation to the ball.
+        Currently, each paddle has its own chunk of code where input is detected by the keyboard; 
+        this feels like an excellent place to put the code we need! Once either the left or right paddle (or both, if desired) 
+        try to deflect the paddle on their own, you’ve done it!
+]]
