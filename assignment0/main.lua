903c220a7805512b12ac46c2d3944df7c96229bc
alice <bow1226@gmail.com>
2020-01-21 14:35:10 +0800

    docs: Update TODO task

--- assignment0/main.lua
+++ assignment0/main.lua
@@ -232,7 +232,7 @@ function love.update(dt)
     --
     -- paddles can move no matter what state we're in
     --
-    -- Set player 1 is an AI-controlled paddle
+    -- player 1
     if love.keyboard.isDown('w') then
         player1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
@@ -241,7 +241,7 @@ function love.update(dt)
         player1.dy = 0
     end
 
-    -- player 2
+    -- pSet player 2 is an AI-controlled paddle
     if love.keyboard.isDown('up') then
         player2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
