16254dd5c87a22bde4fca947ead7cc7f485e5205
Hsin-Wen Chang <bow1226@gmail.com>
2020-08-22 12:02:15 +0800

    feat: Set player 2 is an AI-controlled paddle

--- assignment0/main.lua
+++ assignment0/main.lua
@@ -241,14 +241,8 @@ function love.update(dt)
         player1.dy = 0
     end
 
-    -- player 2
-    if love.keyboard.isDown('up') then
-        player2.dy = -PADDLE_SPEED
-    elseif love.keyboard.isDown('down') then
-        player2.dy = PADDLE_SPEED
-    else
-        player2.dy = 0
-    end
+    -- Set player 2 is an AI-controlled paddle
+    player2.y = ball.y 
 
     -- update our ball based on its DX and DY only if we're in play state;
     -- scale the velocity by dt so movement is framerate-independent
