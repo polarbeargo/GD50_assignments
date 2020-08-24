1796d271f1d242dbae0a1c36a0e1f79f02643a35
Hsin-Wen Chang <bow1226@gmail.com>
2020-08-24 13:04:57 +0800

    feat:feat: Randomize the gap between pipes (vertical space), such that they’re no longer hardcoded to 90 pixels.

--- assignment1/PipePair.lua
+++ assignment1/PipePair.lua
@@ -10,9 +10,6 @@
 
 PipePair = Class{}
 
--- size of the gap between pipes
-local GAP_HEIGHT = 90
-
 function PipePair:init(y)
     -- flag to hold whether this pair has been scored (jumped through)
     self.scored = false
@@ -26,7 +23,7 @@ function PipePair:init(y)
     -- instantiate two pipes that belong to this pair
     self.pipes = {
         ['upper'] = Pipe('top', self.y),
-        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + GAP_HEIGHT)
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + love.math.random(50,300))
     }
 
     -- whether this pipe pair is ready to be removed from the scene
