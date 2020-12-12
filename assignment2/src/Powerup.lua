5c55224eeb79fb84e831f9d1248512737cdd0760
Hsin-Wen Chang <bow1226@gmail.com>
2020-12-12 17:20:35 +0800

    fix: Typo

--- assignment2/src/Powerup.lua
+++ assignment2/src/Powerup.lua
@@ -49,6 +49,6 @@ end
 
 function Powerup:render()
     if self.visible then
-		love.graphics.draw(gTextures['main'], gFrames['powerups'][self.type], self.x, self.y)
+		love.graphics.draw(gTextures['main'], gFrames['powerup'][self.type], self.x, self.y)
 	end
 end
